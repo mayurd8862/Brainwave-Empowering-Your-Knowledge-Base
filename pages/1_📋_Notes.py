98411aca539474cbbeab457b15351696369d32d9
Mayur Balu Dabade <mayurdabade1103@gmail.com>
2024-05-12 01:57:58 +0530

    added notes feature  with mongodb power

--- pages/1_📋_Notes.py
+++ pages/1_📋_Notes.py
@@ -1,70 +1,54 @@
+import pymongo
 import streamlit as st
-import os
 from datetime import datetime
-st.title("✏️📓Create Notes")
 
-def save_notes(notes, folder_name):
-    notes_folder = "Notes"
-    if not os.path.exists(notes_folder):
-        os.makedirs(notes_folder)
-    folder_path = os.path.join(notes_folder, folder_name)
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")  # Add timestamp to filename
-    file_path = os.path.join(folder_path, f"notes_{timestamp}.txt")  # Use timestamp in filename
-    with open(file_path, 'w') as file:  
-        file.write(notes)
+myclient = pymongo.MongoClient("mongodb+srv://mayurdabade1103:HvZ2QBn2XuQYool8@brainwave.bndu2pa.mongodb.net/")
+mydb = myclient["Brainwave"]
 
+def save_content(proj_name,title,content,time_data):
+    # myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    # mydb = myclient["Brainwave"]
+    info ={"Title": title, "Content": content, "time_date" : time_data}
+    mycol = mydb[proj_name]
+    mycol.insert_one(info)
 
-def read_files_from_folder(selected_folder):
-    folder_contents = os.listdir(selected_folder)
-    files = [f for f in folder_contents if os.path.isfile(os.path.join(selected_folder, f))]
-    return files
+if __name__ == '__main__':
 
-
-t1,t2 = st.tabs(['Create Notes','Show Saved Notes'])
-
-with t1:
-
-    # Ask user for folder name
-    folder_name = st.text_input("📁Enter project name to save it as a folder:")
     
-    # Display notes section only if folder name is provided
-    if folder_name:
-        notes = st.text_area("📝Enter your notes here:")
+    t1,t2 = st.tabs(['Create Notes','Show Saved Notes'])
+
+    with t1:
+        project_name = st.text_input("📁Enter project name to save it as a folder:")
+        title = st.text_input("Enter title")
+        note_content = st.text_area("📝Enter your notes here:")
         save_button = st.button("Save")
 
+        current_time = datetime.now()
+        # Format the date and time in a human-readable format
+        time_data = current_time.strftime("%A, %d %B %Y %I:%M:%S %p")
+
         if save_button:
-            save_notes(notes, folder_name)
-            st.success(f"Notes saved successfully in folder: {folder_name}👍")
-
-with t2:
-
-    # Ask user to select a folder
-    notes_folder = "Notes"
-    selected_folder = st.selectbox("Select a folder:", os.listdir(notes_folder))
-
-    if selected_folder:
-        folder_path = os.path.join(notes_folder, selected_folder)
+            save_content(project_name,title,note_content,time_data)
         
-        # Display files and folders in the selected folder
-        folder_contents = os.listdir(folder_path)
-        files = [f for f in folder_contents if os.path.isfile(os.path.join(folder_path, f))]
-        
-        # Show files in dropdown
-        selected_file = st.selectbox("Select a file:", files)
+            st.success("✔️ Saved successfully")
 
-        if selected_file:
-            file_path = os.path.join(folder_path, selected_file)
-            with open(file_path, 'r', encoding='utf-8') as file:
-
-            # with open(file_path, 'r') as file:
-                file_contents = file.read()
-                st.subheader(f"Contents of {selected_file}:")
-                st.write(file_contents)
+    with t2:
+        print(mydb.list_collection_names())
         
-        share = st.button("share")
-    
-    
-   
+        selected_folder = st.selectbox("Select a folder:", mydb.list_collection_names())
+        selected_title = st.selectbox("Select a title:", mydb[selected_folder].distinct("Title"))
+        st.write("Hello")
+
+        if selected_title:
+            # Query MongoDB collection to retrieve document based on selected title
+            document = mydb[selected_folder].find_one({"Title": selected_title})
+
+            # Display content and time_data of selected title
+            if document:
+                st.write("Content:", document["Content"])
+                st.write("Time Date:", document["time_date"])
+            else:
+                st.write("No information found for selected title.")
+
+
    
--- pages/5_🧪_testing.py
+++ pages/5_🧪_testing.py
@@ -83,3 +83,78 @@ if __name__ == '__main__':
             else:
                 st.write("No information found for selected title.")
 
+
+
+
+
+### Saving notes to local directory
+
+
+# import streamlit as st
+# import os
+# from datetime import datetime
+# st.title("✏️📓Create Notes")
+
+# def save_notes(notes, folder_name):
+#     notes_folder = "Notes"
+#     if not os.path.exists(notes_folder):
+#         os.makedirs(notes_folder)
+#     folder_path = os.path.join(notes_folder, folder_name)
+#     if not os.path.exists(folder_path):
+#         os.makedirs(folder_path)
+#     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")  # Add timestamp to filename
+#     file_path = os.path.join(folder_path, f"notes_{timestamp}.txt")  # Use timestamp in filename
+#     with open(file_path, 'w') as file:  
+#         file.write(notes)
+
+
+# def read_files_from_folder(selected_folder):
+#     folder_contents = os.listdir(selected_folder)
+#     files = [f for f in folder_contents if os.path.isfile(os.path.join(selected_folder, f))]
+#     return files
+
+
+# t1,t2 = st.tabs(['Create Notes','Show Saved Notes'])
+
+# with t1:
+
+#     # Ask user for folder name
+#     folder_name = st.text_input("📁Enter project name to save it as a folder:")
+    
+#     # Display notes section only if folder name is provided
+#     if folder_name:
+#         notes = st.text_area("📝Enter your notes here:")
+#         save_button = st.button("Save")
+
+#         if save_button:
+#             save_notes(notes, folder_name)
+#             st.success(f"Notes saved successfully in folder: {folder_name}👍")
+
+# with t2:
+
+#     # Ask user to select a folder
+#     notes_folder = "Notes"
+#     selected_folder = st.selectbox("Select a folder:", os.listdir(notes_folder))
+
+#     if selected_folder:
+#         folder_path = os.path.join(notes_folder, selected_folder)
+        
+#         # Display files and folders in the selected folder
+#         folder_contents = os.listdir(folder_path)
+#         files = [f for f in folder_contents if os.path.isfile(os.path.join(folder_path, f))]
+        
+#         # Show files in dropdown
+#         selected_file = st.selectbox("Select a file:", files)
+
+#         if selected_file:
+#             file_path = os.path.join(folder_path, selected_file)
+#             with open(file_path, 'r', encoding='utf-8') as file:
+
+#             # with open(file_path, 'r') as file:
+#                 file_contents = file.read()
+#                 st.subheader(f"Contents of {selected_file}:")
+#                 st.write(file_contents)
+        
+#         share = st.button("share")
+    
+    
