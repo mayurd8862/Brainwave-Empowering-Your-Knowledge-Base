8e655256411a40b8460184f56e5f2f46628fca7b
Mayur Balu Dabade <mayurdabade1103@gmail.com>
2024-05-18 11:22:26 +0530

    added signIn page

--- pages/5_🧪_testing.py
+++ pages/5_🧪_testing.py
@@ -1,18 +1,39 @@
-from langchain_core.messages import HumanMessage, SystemMessage
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_openai import ChatOpenAI
-# from langchain.chains.summarize import load_summarize_chain
-# import os
-# import tempfile
+import streamlit as st
+import pymongo
 
-# Securely provide your Google API key (not shown here)
-# llm = ChatGoogleGenerativeAI(model="gemini-pro")
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
-# print(llm("who is the prime minister of india"))
+# Create an empty container
+# placeholder = st.empty()
 
-messages = [
-    ("system", "You are a helpful assistant that translates English to French."),
-    ("human", "Translate this sentence from English to French. I love programming."),
-]
-llm.invoke(messages)
+myclient = pymongo.MongoClient("mongodb+srv://mayurdabade1103:HvZ2QBn2XuQYool8@brainwave.bndu2pa.mongodb.net/")
+mydb = myclient["Brainwave"]
+mycol = mydb["Login_Credentials"]
+
+def save_cred(name,mail,pwd):
+    info ={"Name": name, "Mail": mail, "Password" : pwd}
+    mycol.insert_one(info)
+
+
+@st.experimental_dialog("Cast your vote")
+def vote(item):
+    st.write(f"Why is {item} your favorite?")
+    reason = st.text_input("Because...")
+    if st.button("Submit"):
+        st.session_state.vote = {"item": item, "reason": reason}
+        st.rerun()
+
+# if "vote" not in st.session_state:
+#     st.write("Vote for your favorite")
+#     if st.button("A"):
+#         vote("A")
+#     if st.button("B"):
+#         vote("B")
+# else:
+#     f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
+
+def main():
+    st.write("Hello Mayur!!")
+
+
+if __name__ == '__main__':
+    vote("A")
+    main()
