0040280c1f80d28bef9fdcccd958f2e4654128ee
Mayur Balu Dabade <mayurdabade1103@gmail.com>
2024-06-22 19:14:37 +0530

    chat with data faiss

--- pages/5_🧪_testing.py
+++ pages/5_🧪_testing.py
@@ -1,88 +1,232 @@
+# import streamlit as st
+# import asyncio
+# from langchain_community.document_loaders import PyPDFLoader
+# from PyPDF2 import PdfReader
+# from langchain.prompts import PromptTemplate
+# from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain.chains.summarize import load_summarize_chain
+# import os
+# import tempfile
+
+# GOOGLE_API_KEY = st.secrets.GOOGLE_API_KEY
+# llm = None
+
+# # Ensure there is a running event loop
+# def get_or_create_event_loop():
+#     try:
+#         loop = asyncio.get_running_loop()
+#     except RuntimeError:
+#         loop = asyncio.new_event_loop()
+#         asyncio.set_event_loop(loop)
+#     return loop
+
+# loop = get_or_create_event_loop()
+
+# async def initialize_llm():
+#     global llm
+#     llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY)
+
+# # Initialize the LLM asynchronously
+# loop.run_until_complete(initialize_llm())
+
+# headers = {
+#     'GOOGLE_API_KEY': st.secrets["GOOGLE_API_KEY"]
+# }
+
+# map_prompt = """
+# Write a concise summary of the following:
+# "{text}"
+# CONCISE SUMMARY:
+# """
+# map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text"])
+# combine_prompt = """
+# Write a concise summary of the following text delimited by triple backquotes.
+# Return your response in bullet points which covers the key points of the text.
+# Also add emojis wherever needed.
+# `{text}`
+# BULLET POINT SUMMARY:
+# """
+# combine_prompt_template = PromptTemplate(template=combine_prompt, input_variables=["text"])
+
+# def summarize_pdfs_from_folder(pdf_files):
+#     summaries = []
+#     for pdf_file in pdf_files:
+#         # if pdf_file is not None:
+#         with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+#             tmp_file.write(pdf_file.read())
+#             pdf_path = tmp_file.name
+#             loader = PyPDFLoader(pdf_path)
+#             docs = loader.load_and_split()
+#         # with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+#         #     temp_path = temp_file.name
+#         #     temp_file.write(pdf_file.read())
+        
+#         # loader = PyPDFLoader(pdf_files)
+#         # docs = loader.load_and_split()
+        
+#         summary_chain = load_summarize_chain(llm=llm,
+#                                              chain_type='map_reduce',
+#                                              map_prompt=map_prompt_template,
+#                                              combine_prompt=combine_prompt_template,
+#                                              verbose=True)
+
+#         summary = summary_chain.run(docs)
+#         summaries.append(summary)
+#         os.remove(pdf_path)
+    
+#     return summaries
+
+# # Streamlit App
+# st.title("🚀Document Summarizer")
+# pdf_files = st.file_uploader("Upload PDF files", type="pdf", accept_multiple_files=True)
+
+# if pdf_files:
+#     if st.button("Generate Summary"):
+#         summaries = summarize_pdfs_from_folder(pdf_files)
+#         for i, summary in enumerate(summaries, 1):
+#             st.write(f"Summary for PDF {i}:")
+#             st.write(summary)
+
+
+
+
+
+
+
+
+
 import streamlit as st
-import asyncio
+from streamlit_chat import message
+# from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import SentenceTransformerEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
-from PyPDF2 import PdfReader
-from langchain.prompts import PromptTemplate
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.summarize import load_summarize_chain
+import pymongo
+import datetime
 import os
 import tempfile
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_groq import ChatGroq
+from langchain.chains import RetrievalQA
+from langchain_community.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
 
-GOOGLE_API_KEY = st.secrets.GOOGLE_API_KEY
-llm = None
+# loader = TextLoader("../../how_to/state_of_the_union.txt")
+# documents = loader.load()
+# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+# docs = text_splitter.split_documents(documents)
+# embeddings = OpenAIEmbeddings()
+# db = FAISS.from_documents(docs, embeddings)
 
-# Ensure there is a running event loop
-def get_or_create_event_loop():
-    try:
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-    return loop
 
-loop = get_or_create_event_loop()
 
-async def initialize_llm():
-    global llm
-    llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY)
+st.title("🤖💬Chat with data")
 
-# Initialize the LLM asynchronously
-loop.run_until_complete(initialize_llm())
 
-headers = {
-    'GOOGLE_API_KEY': st.secrets["GOOGLE_API_KEY"]
-}
+# Connect to MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["chatbot_database"]
+collection = db["chatbot_history"]
 
-map_prompt = """
-Write a concise summary of the following:
-"{text}"
-CONCISE SUMMARY:
-"""
-map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text"])
-combine_prompt = """
-Write a concise summary of the following text delimited by triple backquotes.
-Return your response in bullet points which covers the key points of the text.
-Also add emojis wherever needed.
-`{text}`
-BULLET POINT SUMMARY:
-"""
-combine_prompt_template = PromptTemplate(template=combine_prompt, input_variables=["text"])
+tab1, tab2 = st.tabs(["Chat with PDF", "Chat with website"])
 
-def summarize_pdfs_from_folder(pdf_files):
-    summaries = []
-    for pdf_file in pdf_files:
-        # if pdf_file is not None:
-        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-            tmp_file.write(pdf_file.read())
-            pdf_path = tmp_file.name
-            loader = PyPDFLoader(pdf_path)
-            docs = loader.load_and_split()
-        # with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-        #     temp_path = temp_file.name
-        #     temp_file.write(pdf_file.read())
-        
-        # loader = PyPDFLoader(pdf_files)
-        # docs = loader.load_and_split()
-        
-        summary_chain = load_summarize_chain(llm=llm,
-                                             chain_type='map_reduce',
-                                             map_prompt=map_prompt_template,
-                                             combine_prompt=combine_prompt_template,
-                                             verbose=True)
+with tab1:
+    groq_api_key= st.secrets.GROQ_API_KEY
+    llm=ChatGroq(groq_api_key=groq_api_key,
+                model_name="Llama3-8b-8192")
 
-        summary = summary_chain.run(docs)
-        summaries.append(summary)
-        os.remove(pdf_path)
+
+    # File uploader widget
+    file = st.file_uploader("Upload PDF File", type=["pdf"])
+    submit = st.checkbox('Submit and chat')
+
+    if submit:
     
-    return summaries
+        # with st.spinner("Creating Vector database..."):
+        # st.write('Creating Vector database...')
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.write(file.read())
+        file_path = temp_file.name
+        temp_file.close()
+        
+        loader = PyPDFLoader(file_path)
+        data = loader.load_and_split()  
+        # loader = WebBaseLoader(url)
+        # data = loader.load()
+        # Split
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1500,
+            chunk_overlap = 150
+        )
+        splits = text_splitter.split_documents(data)
 
-# Streamlit App
-st.title("🚀Document Summarizer")
-pdf_files = st.file_uploader("Upload PDF files", type="pdf", accept_multiple_files=True)
+        # word embedding and storing it in Chroma databases
+        embedding = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+        persist_directory = 'docs/chroma/'
 
-if pdf_files:
-    if st.button("Generate Summary"):
-        summaries = summarize_pdfs_from_folder(pdf_files)
-        for i, summary in enumerate(summaries, 1):
-            st.write(f"Summary for PDF {i}:")
-            st.write(summary)
+        # vectordb = Chroma.from_documents(
+        #     documents=splits,
+        #     embedding=embedding,
+        #     persist_directory=persist_directory
+        # )
+
+        vectordb = FAISS.from_documents(splits, embedding)
+
+
+        if 'responses' not in st.session_state:
+            st.session_state['responses'] = ["How can I assist you?"]
+
+        if 'requests' not in st.session_state:
+            st.session_state['requests'] = []
+
+
+        template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. 
+        {context}
+        Question: {question}
+        Helpful Answer:"""
+        QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],template=template,)
+
+        # container for chat history
+        response_container = st.container()
+        # container for text box
+        textcontainer = st.container()
+
+
+        with textcontainer:
+            query = st.chat_input("Query: ", key="input")
+            if query:
+                with st.spinner("typing..."):
+
+                    qa_chain = RetrievalQA.from_chain_type(llm,
+                                            retriever=vectordb.as_retriever(),
+                                            return_source_documents=True,
+                                            chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
+            
+                    result = qa_chain({"query": query})
+                    response = result["result"]
+
+                st.session_state.requests.append(query)
+                st.session_state.responses.append(response) 
+
+        with response_container:
+            if st.session_state['responses']:
+
+                for i in range(len(st.session_state['responses'])):
+                    message(st.session_state['responses'][i],key=str(i))
+                    if i < len(st.session_state['requests']):
+                        message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+    
+
+
+            
+
+
+with tab2:
+    pass
+
+
+
+
+
